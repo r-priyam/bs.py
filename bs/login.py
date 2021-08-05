dc173515db6f3733318a33e3d1a0d85b07549bae
Priyam <priyamroy29@gmail.com>
2021-08-05 23:33:01 +0530

    login update

--- bs/login.py
+++ bs/login.py
@@ -1,11 +1,8 @@
-import typing
-
 from .client import Client
 
 
 def login(
-        email: str, password: str, client: typing.Type[typing.Optional[Client]] = Client, **kwargs
-) -> typing.Optional[Client]:
-    instance = client(**kwargs)
+        email: str, password: str, **kwargs):
+    instance = Client(**kwargs)
     instance.loop.run_until_complete(instance.login(email, password))
     return instance
