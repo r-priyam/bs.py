285fe73bbadd1b27c95ce22268be228e11f94c6e
Prince2347X <prince.raj29042004@gmail.com>
2021-08-06 12:41:34 +0530

    Some Changes - Updated setup.py - Changed README.rst to README.md - Added templates in .gitignore

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # bs.py
 
-### ! W.I.P !
+### ! Work In Progress !
 
 Async python wrapper for the brawl stars api.
 Some HTTP codes are taken from coc.py.
--- setup.py
+++ setup.py
@@ -1,34 +1,40 @@
 import os
+import re
 import subprocess
 
+import setuptools
 from setuptools import setup
 
-REQUIREMENTS = []
 with open(os.path.join(os.getcwd(), "requirements.txt")) as f:
     REQUIREMENTS = f.read().splitlines()
 
-VERSION = "0.0.1"
+
+with open('bs/__init__.py') as f:
+    VERSION = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 if "a" in VERSION:
     VERSION += "+" + subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("utf-8").strip()
 
-README = ""
-with open("README.rst") as f:
+with open("README.md") as f:
     README = f.read()
 
 setup(
     name="bs.py",
     author="priyamroy",
     url="https://github.com/priyamroy/bs.py",
-    packages=["bs"],
+    packages=setuptools.find_packages(),
     version=VERSION,
     license="MIT",
     description="A python wrapper for the Brawl Stars API",
     long_description=README,
+    long_description_content_type="text/markdown",
+    keywords=['brawl stars', 'bs api', 'brawl stars python', 'brawl stars api'],
     python_requires=">=3.6.0",
     install_requires=REQUIREMENTS,
     classifiers={
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     }
 )
