d62e019767f5bde6468f08ae6d974d86e84c7ace
Priyam <priyamroy29@gmail.com>
2021-08-06 17:01:47 +0530

    player methods

--- bs/client.py
+++ bs/client.py
@@ -1,7 +1,10 @@
-from .http import HTTPClient, BasicThrottler, BatchThrottler
-import logging
 import asyncio
+import logging
 from typing import Type, Union
+
+from .http import BasicThrottler, BatchThrottler, HTTPClient
+from .players import Player
+from .utils import correct_tag
 
 KEY_MINIMUM, KEY_MAXIMUM = 1, 10
 LOG = logging.getLogger(__name__)
@@ -11,26 +14,26 @@ class Client:
     """Client connection to interact with the api."""
 
     def __init__(
-            self,
-            *,
-            key_count: int = 1,
-            key_names: str = "Created with bs.py Client",
-            throttle_limit: int = 10,
-            loop: asyncio.AbstractEventLoop = None,
-            correct_tags: bool = True,
-            throttler: Type[Union[BasicThrottler, BatchThrottler]] = BasicThrottler,
-            connector=None,
-            timeout: float = 30.0,
-            cache_max_size: int = 10000,
-            stats_max_size: int = 1000,
-            **_,
+        self,
+        *,
+        key_count: int = 1,
+        key_names: str = "Created with bs.py Client",
+        throttle_limit: int = 10,
+        loop: asyncio.AbstractEventLoop = None,
+        correct_tags: bool = True,
+        throttler: Type[Union[BasicThrottler, BatchThrottler]] = BasicThrottler,
+        connector=None,
+        timeout: float = 30.0,
+        cache_max_size: int = 10000,
+        stats_max_size: int = 1000,
+        **_,
     ):
         self.loop = loop or asyncio.get_event_loop()
 
         self.correct_key_count = max(min(KEY_MAXIMUM, key_count), KEY_MINIMUM)
 
-        if not key_count == self.correct_key_count:
-            raise RuntimeError("Key count must be within {}-{}".format(KEY_MINIMUM, KEY_MAXIMUM))
+        if key_count != self.correct_key_count:
+            raise RuntimeError(f"Key count must be within {KEY_MINIMUM}-{KEY_MAXIMUM}")
 
         self.key_names = key_names
         self.throttle_limit = throttle_limit
@@ -67,3 +70,48 @@ class Client:
         await self._ready.wait()
         self._ready.clear()
         LOG.debug("HTTP connection created. Client is ready for use.")
+
+    def close(self) -> None:
+        if self.http:
+            LOG.info("Brawl Stars client logging out...")
+            self.dispatch("on_client_close")
+            self.loop.run_until_complete(self.http.close())
+            self.loop.close()
+
+    def dispatch(self, event_name: str, *args, **kwargs) -> None:
+        LOG.debug("Dispatching %s event", event_name)
+
+        try:
+            fctn = getattr(self, event_name)
+        except AttributeError:
+            return
+
+        if asyncio.iscoroutinefunction(fctn):
+            self.loop.create_task(fctn(*args, **kwargs))
+        else:
+            fctn(*args, **kwargs)
+
+    async def reset_keys(self, number_of_keys: int = None) -> None:
+        self._ready.clear()
+        if self.http and self.http._keys:
+            num = number_of_keys or len(self.http._keys)
+            keys = self.http._keys
+            for i in range(num):
+                await self.http.reset_key(keys[i])
+            self._ready.set()
+
+    async def get_player_battle_log(self, player_tag: str):
+        if self.correct_tags:
+            player_tag = correct_tag(player_tag)
+
+        if self.http:
+            data = await self.http.get_player_battle_log(player_tag)
+            return data
+
+    async def get_player(self, player_tag: str):
+        if self.correct_tags:
+            player_tag = correct_tag(player_tag)
+
+        if self.http:
+            data = await self.http.get_player(player_tag)
+            return Player(data=data, client=self.http)
